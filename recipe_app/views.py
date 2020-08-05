6d9c6f98686aa660b9ff87b57bdf99e910f3f7b0
Kathryn Anderson <kat42272@yahoo.com>
2020-08-05 12:10:55 -0700

    Set up urls views settings to show base page text

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  context = {
+    "author": "Author",
+    "article": "Article",
+  }
+  template_name = "index.html"
+  return render(request, template_name, context)
