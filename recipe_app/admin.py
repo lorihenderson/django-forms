bf6d2b97da533b7af8f77165fd4f7a2f7361bd95
Kathryn Anderson <kat42272@yahoo.com>
2020-08-10 10:21:36 -0700

    Initial Commit

--- recipe_app/admin.py
+++ recipe_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from recipe_app.models import Author, Recipe
 
 # Register your models here.
+# Coded and written by Kathryn Anderson
 
 admin.site.register(Author)
 admin.site.register(Recipe)
